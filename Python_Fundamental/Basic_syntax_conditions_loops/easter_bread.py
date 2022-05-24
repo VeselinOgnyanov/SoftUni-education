741ba4e9b01aa39269ee917982e771458eb7034b
Veselin <veskospasov91@abv.bg>
2022-05-24 19:36:17 +0300

    task "Easter_bread" added

--- Python_Fundamental/Basic_syntax_conditions_loops/easter_bread.py
+++ Python_Fundamental/Basic_syntax_conditions_loops/easter_bread.py
@@ -7,9 +7,18 @@ count_loafs = 0
 
 price_for_1_pack_of_eggs = price_for_1_kg_flour * 0.75
 price_for_1_liter_milk = price_for_1_kg_flour * 1.25
+price_for_one_loaf = 0
 
-while budget > 0:
+while budget >= price_for_one_loaf:
     price_for_one_loaf = price_for_1_pack_of_eggs + price_for_1_kg_flour + (price_for_1_liter_milk / 4)
+    if price_for_one_loaf >= budget:
+        break
     colored_eggs += 3
+    count_loafs += 1
+    if count_loafs % 3 == 0:
+        loosed_eggs = count_loafs - 2
+        colored_eggs -= loosed_eggs
     budget -= price_for_one_loaf
+
+print(f"You made {count_loafs} loaves of Easter bread! Now you have {colored_eggs} eggs and {budget:.2f}BGN left.")
 
