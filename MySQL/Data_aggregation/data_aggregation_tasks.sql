c7b9ea8928d1d062211b74a74db8106cb352dfc9
Veselin <veskospasov91@abv.bg>
2023-06-06 18:02:22 +0300

    new

--- MySQL/Data_aggregation/data_aggregation_tasks.sql
+++ MySQL/Data_aggregation/data_aggregation_tasks.sql
@@ -1,7 +1,150 @@
 -- LAB
 -- 1. Departments Info
+SELECT department_id, COUNT(department_id) FROM employees
+GROUP BY department_id;
 
-SELECT `id`, `first_name`,
-`last_name`, `department_id`, MAX(`salary`) AS 'max_salary' FROM `employees`
-GROUP BY `department_id`;
 
+-- 2. Average Salary
+SELECT department_id, ROUND(AVG(salary), 2) AS 'Average salary' FROM employees
+GROUP BY department_id
+ORDER BY department_id;
+
+-- 3. Minimum Salary 
+SELECT department_id, ROUND(MIN(salary), 2) AS 'Min salary' FROM employees
+GROUP BY department_id
+HAVING `Min salary` > 800
+ORDER BY department_id;
+
+-- 4. Appetizers Count 
+SELECT COUNT(name) FROM products
+WHERE category_id = 2 AND price > 8;
+
+-- 5. Menu Prices
+SELECT category_id, ROUND(AVG(price), 2) AS 'Average Price', ROUND(MIN(price), 2) AS 'Cheapest Produc',
+ ROUND(MAX(price), 2) AS 'Most Expensive Product'
+FROM products
+GROUP BY category_id;
+
+-- Exercise
+-- 01. Records’ Count 
+SELECT COUNT(id) AS 'count' FROM wizzard_deposits;
+
+-- 02. Longest Magic Wand
+SELECT MAX(magic_wand_size) AS 'longest_magic_wand' FROM wizzard_deposits;
+
+-- 03. Longest Magic Wand per Deposit Groups 
+SELECT deposit_group, MAX(magic_wand_size) AS 'longest_magic_wand' FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY `longest_magic_wand`, deposit_group;
+
+-- 04. Smallest Deposit Group per Magic Wand Size
+SELECT deposit_group FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY MIN(magic_wand_size)
+LIMIT 1;
+
+-- 05. Deposits Sum 
+SELECT deposit_group, SUM(deposit_amount) AS 'total_sum' FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY `total_sum`;
+
+-- 06. Deposits Sum for Ollivander Family 
+SELECT deposit_group, SUM(deposit_amount) AS 'total_sum' FROM wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+ORDER BY deposit_group ASC;
+
+-- 07. Deposits Filter 
+SELECT deposit_group, SUM(deposit_amount) AS 'total_sum' FROM wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+HAVING `total_sum` < 150000
+ORDER BY total_sum DESC;
+
+-- 08. Deposit Charge 
+SELECT deposit_group, magic_wand_creator, MIN(deposit_charge) AS 'min_deposit_charge' FROM wizzard_deposits
+GROUP BY deposit_group, magic_wand_creator
+ORDER BY magic_wand_creator, deposit_group;
+
+-- 09. Age Groups
+SELECT 
+CASE
+WHEN age <= 10 THEN '[0-10]'
+WHEN age <= 20 THEN '[11-20]'
+WHEN age <= 30 THEN '[21-30]'
+WHEN age <= 40 THEN '[31-40]'
+WHEN age <= 50 THEN '[41-50]'
+WHEN age <= 60 THEN '[51-60]'
+ELSE '[61+]'
+END AS 'age_group', COUNT(`age`) AS 'wizzard_count' FROM wizzard_deposits
+GROUP BY `age_group`
+ORDER BY `age_group`;
+
+-- 10. First Letter
+SELECT DISTINCT(SUBSTRING(first_name, 1, 1)) AS 'first_letter'FROM wizzard_deposits
+WHERE deposit_group = 'Troll Chest'
+ORDER BY `first_letter`;
+
+-- 11. Average Interest 
+SELECT deposit_group, is_deposit_expired, AVG(deposit_interest) AS 'average_interest' FROM wizzard_deposits
+WHERE deposit_start_date > '1985-01-01'
+GROUP BY deposit_group, is_deposit_expired
+ORDER BY deposit_group DESC, is_deposit_expired ASC;
+
+-- 12. Employees Minimum Salaries
+SELECT department_id, MIN(salary) FROM employees
+WHERE DATE(hire_date) > '2000-01-01'
+GROUP BY department_id
+HAVING department_id IN (2, 5, 7)
+ORDER BY department_id ASC;
+
+-- 13. Employees Average Salaries 
+CREATE TABLE `high_paid_employees_over_30000` AS
+SELECT * FROM employees
+WHERE salary > 30000;
+
+DELETE FROM `high_paid_employees_over_30000`
+WHERE manager_id  = 42;
+
+UPDATE `high_paid_employees_over_30000`
+SET salary = salary + 5000
+WHERE department_id = 1;
+
+SELECT department_id, AVG(salary) AS 'avg_salary' FROM `high_paid_employees_over_30000`
+GROUP BY department_id
+ORDER BY department_id ASC;
+
+-- 14. Employees Maximum Salaries 
+SELECT department_id, MAX(salary) AS 'max_salary' FROM employees
+GROUP BY department_id
+HAVING `max_salary` < 30000 OR `max_salary` > 70000
+ORDER BY department_id ASC; 
+
+-- 15. Employees Count Salaries 
+SELECT COUNT(employee_id) AS 'count' FROM employees
+WHERE manager_id IS NULL; 
+
+-- 16. 3rd Highest Salary
+SELECT DISTINCT department_id, (
+    SELECT DISTINCT salary FROM employees AS e1
+    WHERE e1.department_id = employees.department_id
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 2
+    ) AS 'third_highest_salary'
+FROM employees
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY department_id ASC;
+
+-- 17. Salary Challenge 
+SELECT first_name, last_name, department_id FROM employees AS e1
+WHERE salary > (
+	SELECT AVG(salary) FROM employees AS e2
+    WHERE e1.department_id = e2.department_id
+)
+ORDER BY department_id, employee_id
+LIMIT 10;
+
+-- 18. Departments Total Salaries
+SELECT department_id, SUM(salary) AS 'total_SUM' FROM employees
+GROUP BY department_id
+ORDER BY department_id;
---
+++ MySQL/Subqueries_and_JOINs/subqueries_and_joins.sql
@@ -0,0 +1,161 @@
+-- LAB
+-- 1. Managers 
+SELECT e.employee_id, CONCAT_WS(' ', e.first_name, e.last_name) AS 'full_name',
+		d.department_id, d.name FROM employees AS e
+RIGHT JOIN departments AS d
+ON d.manager_id = e.employee_id
+ORDER BY e.employee_id
+LIMIT 5;
+
+-- 2. Towns and Addresses 
+SELECT t.town_id, t.`name`, a.address_text FROM towns AS t
+RIGHT JOIN addresses AS a
+ON t.town_id = a.town_id
+WHERE `name` IN ('Sofia', 'San Francisco', 'Carnation');
+
+-- 3. Employees Without Managers
+SELECT employee_id, first_name, last_name,department_id, ROUND(salary) AS 'salary'
+FROM employees
+WHERE manager_id IS NULL;
+
+-- 4. High Salary
+SELECT COUNT(*) FROM employees
+WHERE salary > (SELECT AVG(salary)FROM employees);
+
+-- Exercises 
+-- 01. Employee Address
+SELECT e.employee_id, e.job_title, e.address_id, a.address_text 
+FROM employees AS e
+JOIN addresses AS a ON e.address_id = a.address_id
+ORDER BY address_id
+LIMIT 5;
+
+-- 02. Addresses with Towns 
+SELECT e.first_name, e.last_name, t.`name` AS 'town_name', a.address_text FROM employees AS e
+JOIN addresses AS a
+ON e.address_id = a.address_id
+JOIN towns AS t
+ON a.town_id  = t.town_id
+ORDER BY e.first_name ASC, e.last_name
+LIMIT 5;
+
+-- 03. Sales Employee 
+SELECT e.employee_id, e.first_name, e.last_name,
+d.name AS 'department_name' FROM employees AS e
+JOIN departments AS d
+ON e.department_id = d.department_id
+WHERE d.name = 'Sales'
+ORDER BY e.employee_id DESC;
+
+-- 04. Employee Departments 
+SELECT e.employee_id, e.first_name, e.salary,
+d.name AS 'department_name' FROM employees AS e
+JOIN departments AS d ON e.department_id = d.department_id
+WHERE salary > 15000
+ORDER BY e.department_id DESC
+LIMIT 5;
+
+-- 05. Employees Without Project
+SELECT e.employee_id, first_name FROM employees AS e
+LEFT JOIN employees_projects AS ep ON e.employee_id = ep.employee_id 
+WHERE ep.employee_id IS NULL 
+ORDER BY e.employee_id DESC
+LIMIT 3;
+
+-- 06. Employees Hired After 
+SELECT e.first_name, e.last_name,
+e.hire_date, d.name AS 'dept_name' FROM employees AS e
+JOIN departments AS d
+ON e.department_id = d.department_id
+WHERE DATE(e.hire_date) > '1999-01-01' AND d.`name` IN ('Sales', 'Finance')
+ORDER BY e.hire_date ASC;
+
+-- 07. Employees with Project 
+SELECT e.employee_id, e.first_name, p.`name` AS 'project_name' FROM employees AS e
+JOIN employees_projects AS ep ON e.employee_id = ep.employee_id
+JOIN projects AS p ON ep.project_id = p.project_id
+WHERE DATE(p.start_date) > '2002-08-13' AND p.end_date IS NULL
+ORDER BY e.first_name ASC, p.name ASC
+LIMIT 5;
+
+-- 8. Employee 24
+SELECT e.employee_id, e.first_name, 
+IF(YEAR(p.start_date) >= 2005, NULL, p.name) AS `project_name`
+FROM employees AS e
+JOIN employees_projects AS ep ON e.employee_id = ep.employee_id
+JOIN projects AS p ON ep.project_id = p.project_id
+WHERE e.employee_id = 24
+ORDER BY e.first_name ASC, p.name ASC
+LIMIT 5;
+
+-- 09. Employee Manager 
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.manager_id,
+    m.first_name AS 'manager_name'
+FROM
+    employees AS e
+        JOIN
+    employees m ON e.manager_id = m.employee_id
+WHERE
+    e.manager_id IN (3 , 7)
+    ORDER BY e.first_name;
+    
+-- 10. Employee Summary 
+SELECT e.employee_id, CONCAT_WS(' ', e.first_name, e.last_name) AS 'employee_name',
+CONCAT_WS(' ', m.first_name, m.last_name) AS manager_name, d.name AS 'department_name'
+FROM employees AS e
+JOIN employees AS m ON m.employee_id = e.manager_id
+JOIN departments AS d ON e.department_id = d.department_id
+ORDER BY e.employee_id
+LIMIT 5;
+
+-- 11. Min Average Salary
+SELECT AVG(salary) AS min_average_salary FROM employees
+GROUP BY department_id
+ORDER BY min_average_salary
+LIMIT 1;
+
+-- 12. Highest Peaks in Bulgaria
+SELECT c.country_code, m.mountain_range, p.peak_name, p.elevation FROM countries AS c
+JOIN mountains_countries AS mc ON c.country_code = mc.country_code
+JOIN peaks AS p ON mc.mountain_id = p.mountain_id
+JOIN mountains AS m ON m.id = mc.mountain_id
+WHERE c.country_code = 'BG'
+AND p.elevation > 2835
+ORDER BY p.elevation DESC;
+
+-- 13. Count Mountain Ranges
+SELECT c.country_code, COUNT(m.mountain_range) AS 'mountain_range' FROM countries AS c
+JOIN mountains_countries AS mc ON c.country_code = mc.country_code
+JOIN mountains AS m ON mc.mountain_id = m.id
+GROUP BY c.country_code
+HAVING c.country_code IN ('US', 'RU', 'BG')
+ORDER BY `mountain_range` DESC;
+
+-- 14. Countries with Rivers
+SELECT c.country_name, r.river_name FROM countries AS c
+LEFT JOIN countries_rivers AS cr ON c.country_code = cr.country_code
+LEFT JOIN rivers AS r ON cr.river_id = r.id
+WHERE c.continent_code = 'AF'
+ORDER BY c.country_name
+LIMIT 5;
+
+-- 15. *Continents and Currencies
+-- 16. Countries Without Any Mountains
+SELECT COUNT(c.country_code) AS 'country_count' FROM countries AS c
+LEFT JOIN mountains_countries AS mc ON c.country_code = mc.country_code
+WHERE mc.mountain_id IS NULL;
+
+-- 17. Highest Peak and Longest River by Country
+SELECT c.country_name, MAX(p.elevation) AS 'highest_peak_elevation',
+MAX(r.length) AS 'longest_river_length' FROM countries AS c
+JOIN mountains_countries AS mc ON c.country_code = mc.country_code
+JOIN countries_rivers AS cr ON c.country_code = cr.country_code
+JOIN peaks AS p ON mc.mountain_id = p.mountain_id
+JOIN rivers AS r ON cr.river_id = r.id
+GROUP BY c.country_name
+ORDER BY `highest_peak_elevation` DESC, `longest_river_length` DESC,
+c.country_name ASC
+LIMIT 5;
