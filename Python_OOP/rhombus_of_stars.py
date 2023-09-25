7c938f651754adcb88dd610553af6ea885195abb
VeselinOgnyanov <veskospasov91@abv.bg>
2023-09-25 23:08:40 +0300

    new

--- Python_OOP/rhombus_of_stars.py
+++ Python_OOP/rhombus_of_stars.py
@@ -1,1 +1,7 @@
-print("test")
+rhombus_number = int(input())
+
+for cur_time in range(rhombus_number + 1):
+    print(((rhombus_number - cur_time) * " ") + (cur_time * "* "))
+
+for cur_time in range(rhombus_number - 1, 0, -1):
+    print(((rhombus_number - cur_time) * " ") + cur_time * "* ")
