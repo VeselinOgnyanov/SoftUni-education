0125e5b51269b2f69d42c31582071e3fb84d3a77
VeselinOgnyanov <veskospasov91@abv.bg>
2023-10-07 13:18:20 +0300

    Remove deleted files

--- Python_OOP/rhombus_of_stars.py
+++
@@ -1,7 +0,0 @@
-rhombus_number = int(input())
-
-for cur_time in range(rhombus_number + 1):
-    print(((rhombus_number - cur_time) * " ") + (cur_time * "* "))
-
-for cur_time in range(rhombus_number - 1, 0, -1):
-    print(((rhombus_number - cur_time) * " ") + cur_time * "* ")
