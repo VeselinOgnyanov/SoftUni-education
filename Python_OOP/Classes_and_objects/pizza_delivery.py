cca89599048f6b534372ed9283b1402ad55396aa
VeselinOgnyanov <veskospasov91@abv.bg>
2023-11-02 00:31:29 +0200

    new

---
+++ Python_OOP/Classes_and_objects/pizza_delivery.py
@@ -0,0 +1,51 @@
+class PizzaDelivery:
+    def __init__(self, name: str, price: float, ingredients: dict) -> None:
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+        self.ordered = False
+
+    def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
+        if self.ordered_pizza():
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+        if ingredient in self.ingredients:
+            self.ingredients[ingredient] += quantity
+        else:
+            self.ingredients[ingredient] = 0
+            self.ingredients[ingredient] += quantity
+        self.price += quantity * price_per_quantity
+
+    def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
+        if self.ordered_pizza():
+            return "Pizza " + self.name + " already prepared, and we can't make any changes!"
+        if ingredient not in self.ingredients:
+            return "Wrong ingredient selected! We do not use " + ingredient + " in " + self.name + "!"
+        elif ingredient in self.ingredients:
+            if self.ingredients[ingredient] < quantity:
+                return f"Please check again the desired quantity of {ingredient}!"
+            else:
+                self.ingredients[ingredient] -= quantity
+                self.price -= (quantity * price_per_quantity)
+
+    def make_order(self):
+        self.ordered = True
+        list_of_ingrediants = []
+        for key, value in self.ingredients.items():
+            list_of_ingrediants.append(f"{key}: {value}")
+        string_of_ingredients = ", ".join(list_of_ingrediants)
+        return f"You've ordered pizza {self.name} prepared with {string_of_ingredients} and the price will be {self.price}lv."
+
+    def ordered_pizza(self):
+        if self.ordered:
+            return "Pizza " + self.name + " already prepared, and we can't make any changes!"
+
+
+margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
+margarita.add_extra('mozzarella', 1, 0.5)
+margarita.add_extra('cheese', 1, 1)
+margarita.remove_ingredient('cheese', 1, 1)
+print(margarita.remove_ingredient('bacon', 1, 2.5))
+print(margarita.remove_ingredient('tomatoes', 2, 0.5))
+margarita.remove_ingredient('cheese', 2, 1)
+print(margarita.make_order())
+print(margarita.add_extra('cheese', 1, 1))
